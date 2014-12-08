392bcf6d885379b1ad3fb64c93c3e2443f85d6c2
Sergej Petrovski <sergej.petrovski@yahoo.com>
2014-12-08 14:45:24 +0100

    preview classes

---
+++ wpfcm1/Preview/PreviewView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace wpfcm1.Preview
+{
+    public partial class PreviewView : UserControl
+    {
+        public PreviewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ wpfcm1/Preview/PreviewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Caliburn.Micro;
+
+namespace wpfcm1.Preview
+{
+    public interface IPreview { }
+
+    [Export(typeof(IPreview)), PartCreationPolicy(CreationPolicy.Shared)]
+    public class PreviewViewModel : PropertyChangedBase
+    {
+        public PreviewViewModel()
+        {
+            
+        }
+    }
+}
