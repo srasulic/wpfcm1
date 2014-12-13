bb1b112c47749d9c322736cba22ec7fb6a531247
Sergej Petrovski <sergej.petrovski@yahoo.com>
2014-12-13 14:28:13 +0100

    button tooltips

--- wpfcm1/Toolbar/ToolBarViewModel.cs
+++ wpfcm1/Toolbar/ToolBarViewModel.cs
@@ -13,14 +13,14 @@ namespace wpfcm1.Toolbar
         public ToolBarViewModel(IEventAggregator events)
         {
             Buttons = new BindableCollection<ToolBarButtonViewModel>();
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_home_empty", new MessageShowHome()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_refresh", new MessageSync()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_search", new MessagePreview()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_edit", new MessageSign()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_upload", new MessageExtractData()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_delete", new MessageReject()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_check", new MessageValidate()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_arrow", new MessageAck()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_home_empty", "Početni ekran", new MessageShowHome()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_refresh", "Sinhronizacija", new MessageSync()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_search", "Pregled", new MessagePreview()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_edit", "Potpisivanje", new MessageSign()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_upload", "Izvlačenje podataka", new MessageExtractData()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_delete", "Odbacivanje", new MessageReject()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_check", "Validacija potpisa", new MessageValidate()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_arrow", "Potvrda", new MessageAck()));
 
             _activeButtons = new ButtonVisibilityManager(this, events);
         }
