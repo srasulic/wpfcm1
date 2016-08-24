455097e691311489d7c61fe7208b608795e78e24
Srdjan Rasulic <srdjan.rasulic@aserta.rs>
2016-08-24 09:26:28 +0200

    Kozmetika...

--- wpfcm1/Toolbar/ToolBarViewModel.cs
+++ wpfcm1/Toolbar/ToolBarViewModel.cs
@@ -14,14 +14,14 @@ namespace wpfcm1.Toolbar
         {
             Buttons = new BindableCollection<ToolBarButtonViewModel>();
             Buttons.Add(new ToolBarButtonViewModel(events, "appbar_home_empty", "Početni ekran", new MessageShowHome()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_refresh", "Sinhronizacija", new MessageSync()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_search", "Pregled", new MessageTogglePreview()));
-             Buttons.Add(new ToolBarButtonViewModel(events, "appbar_draw_pen", "Potpisivanje", new MessageSign()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_list_gear", "Obrada liste - Izvlačenje podataka", new MessageExtractData()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_delete", "Odbacivanje", new MessageReject()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_list_gear", "Obrada liste - Validacija potpisa", new MessageValidate()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_reply_email", "Potvrda", new MessageAck()));
-            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_office_excel", "Izvezi clipboard u Excel", new MessageXls()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_arrow_down_up", "Pošalji i primi dokumente", new MessageSync()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_search", "Pregled dokumenta", new MessageTogglePreview()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_draw_pen_reflection", "Potpisivanje (potpiši čekirane dokumente iz liste)", new MessageSign()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_list_gear", "Obrada - Izvlačenje podataka", new MessageExtractData()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_page_delete", "Odbacivanje dokumenta (izbaci iz obrade čekirane dokumente iz liste)", new MessageReject()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_list_gear", "Obrada - Validacija potpisa", new MessageValidate()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_reply_email", "Potvrda prijema dokumenta (pošalji povratnicu)", new MessageAck()));
+            Buttons.Add(new ToolBarButtonViewModel(events, "appbar_office_excel", "Izvezi listu dokumenata u Excel", new MessageXls()));
 
             _activeButtons = new ButtonVisibilityManager(this, events);
         }
