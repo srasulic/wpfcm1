6150f9b5027b437fa0c17cbe33fb8d433fb18388
Srdjan Rasulic <srdjan.rasulic@aserta.rs>
2017-04-20 12:59:10 +0200

    Dodat Info (web) sadrzaj; Sinhronizacija xml poruka; Dodat Watcher_Changed kojim setujemo hasExternalMessage za dokumente kojima se promenio xml

--- wpfcm1/FTP/FtpClient.cs
+++ wpfcm1/FTP/FtpClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ namespace wpfcm1.FTP
 {
     public class FtpClient
     {
-        //private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public FtpClient(string uri, string user, string pass)
         {
@@ -49,6 +50,45 @@ namespace wpfcm1.FTP
         }
 
         /// <summary>
+        /// List FTP directory details
+        /// </summary>
+        /// <param name="ftpDir">FTP directory to list (e.g. edokument/faktura/outbound/outbox/)</param>
+        /// <returns></returns>
+        /// NAPOMENA: odustali smo od asinhrone funkcije jer nije pouzdano vraćala listu fajlova. Sekla je rezultate.
+        ///           Kroz debugger je radila ispravno... 
+        public IEnumerable<string> ListDirectoryDetails(string ftpDir = "")
+        {
+            var uri = string.Format("{0}/{1}", Uri, ftpDir);
+
+            var result = new List<string>();
+            var req = (FtpWebRequest)WebRequest.Create(uri);
+            req.Proxy = null;
+            req.Credentials = new NetworkCredential(User, Pass);
+            req.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            try
+            {
+                string lin;
+                using (var resp = req.GetResponse())
+                using (var reader = new StreamReader(resp.GetResponseStream()))
+                {
+
+                    while ((lin = reader.ReadLine()) != null)
+                    //while (reader.Peek() >= 0)
+                    {
+//                        var l = await reader.ReadLineAsync();
+                        result.Add(lin);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("ERR: ListDirectoryDetails", ex);
+            }
+            return result;
+        }
+
+        /// <summary>
         /// Download file from FTP
         /// </summary>
         /// <param name="dir">FTP directory to download from</param>
@@ -59,7 +99,6 @@ namespace wpfcm1.FTP
         {
             var wc = new WebClient {Proxy = null, Credentials = new NetworkCredential(User, Pass)};
             var uri = string.Format(@"{0}{1}{2}", Uri, dir, fileName);
-
             await wc.DownloadFileTaskAsync(uri, destFilePath);
         }
 
---
+++ wpfcm1/FolderGroups/WebView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace wpfcm1.FolderGroups
+{
+    public partial class WebView : UserControl
+    {
+        public WebView()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
